95be5c7ab69c4e0bd34b8aeffae5269813231ade
ross0508 <rossdavidson02@gmail.com>
2025-05-23 12:40:29 +0100

    Add Decks Page

---
+++ src/Decks/Decks.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Decks.css'
+
+const handleAddDeck = () => {
+    // Add Deck
+}
+
+const handleRemoveDeck = (deckId) => {
+    // Remove Deck
+}
+
+export default function Decks() {
+  return (
+    <div className='title'>
+      <h1 className='title'>Decks</h1>
+      <button className='decks-button' onClick={handleAddDeck}>Add Deck</button>
+    </div>
+  )
+}
